d009fc33d1797b37e83b8b4b89b5b168c34d3662
Nicolas Ponce <nponcech@gmail.com>
2023-05-05 09:54:02 -0400

    primer commit

---
+++ src/test/java/com/nicolas/convertirfecha/ConvertirfechaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicolas.convertirfecha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvertirfechaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
